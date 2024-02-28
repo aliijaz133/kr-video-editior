69160d879c14b66b17dddcc6a2d275b263ada9f7
aliijaz133 <aliejaz0072@gmail.com>
2024-02-28 19:44:05 +0500

    Updated

---
+++ kr-studio.py
@@ -0,0 +1,8 @@
+import os
+import subprocess
+
+def run_main():
+    subprocess.Popen(["python", "main.py"], cwd=os.path.dirname(os.path.abspath(__file__)))
+
+if __name__ == "__main__":
+    run_main()
